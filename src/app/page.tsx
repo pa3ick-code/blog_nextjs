cde19d035db51755cba260f5c478e4fcc3914197
patthrills <asubiaropatrick@gmail.com>
2024-04-16 13:05:48 +0100

    Fixed delayed loading problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import Posts from "./components/Posts";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <h1 className="header_title">The Blog</h1>
-      <Posts category="general" max="6"/>
-      <Posts category="technology" max="3" title="Technology"/>
-      <Posts category="business" max="3" title="Business"/>
-      <Posts category="science" max="3" title="Science"/>
-      <Posts category="health" max="3" title="Health"/>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Posts category="general" max="6"/>
+      </Suspense>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Posts category="technology" max="3" title="Technology"/>
+      </Suspense>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Posts category="business" max="3" title="Business"/>
+      </Suspense>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Posts category="science" max="3" title="Science"/>
+      </Suspense>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Posts category="health" max="3" title="Health"/>
+      </Suspense>
     </main>
   );
 }
--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -12,7 +12,7 @@ export default async function getPosts({category, max}: Props) {
     });
 
     const url = `https://gnews.io/api/v4/top-headlines?${searchParams}&apikey=${process.env.API_KEY}`;
-    const getData = await fetch(url, {next: {revalidate: 1000}});
+    const getData = await fetch(url);
     const res = await getData.json();
     const data = await res.articles
     

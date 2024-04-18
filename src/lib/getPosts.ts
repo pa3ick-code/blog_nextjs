f4615093c8d5d0d88aaa041e3d57c8eb636730ca
Pa3ick <66882379+pa3ick-code@users.noreply.github.com>
2024-04-18 01:33:35 +0100

    Update getPosts.ts

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -12,7 +12,7 @@ export default async function getPosts({category, max}: Props) {
     });
 
     const url = `https://gnews.io/api/v4/top-headlines?${searchParams}&apikey=${process.env.API_KEY}`;
-    const getData = await fetch(URL, {next: {revalidate: 480}});
+    const getData = await fetch(url, {next: {revalidate: 480}});
     const res = await getData.json();
     const data = await res.articles;
     

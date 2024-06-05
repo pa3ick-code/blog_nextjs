5967612887ba61028c48661fdc72874c968acf6c
Pa3ick <66882379+pa3ick-code@users.noreply.github.com>
2024-06-05 02:52:44 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,22 @@ import Posts from "./components/Posts";
 import { Suspense } from "react";
 
 export default function Home() {
+  const fetchInfo[] = [
+    {category: "general", max: 6},
+    {category: "technology", max: 3 },
+    {category: "business", max: 3 },
+    {category: "science", max: 3 },
+    {category: "health", max: 3 },
+  ] 
   const content = (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1 className="header_title">The Blog</h1>
-      <Suspense fallback={<h3>Loading...</h3>}>
-        <Posts category="general" max="6"/>
-        <Posts category="technology" max="3" />
-        <Posts category="business" max="3" />
-        <Posts category="science" max="3" />
-        <Posts category="health" max="3" />
-      </Suspense>
+      {
+        fetchInfo.map(({category, max}) => (
+          <Suspence fallback={<h3>Loading...</h3>}>
+            <Posts category={category} max={max}/>
+          </Suspence>
+        ))
+      }
     </main>
   )
 

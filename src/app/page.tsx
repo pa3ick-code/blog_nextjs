34f4b43d8c33746da355c548f5b456d90354e94a
Pa3ick <66882379+pa3ick-code@users.noreply.github.com>
2024-06-05 03:15:22 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 import Posts from "./components/Posts";
+type MyPost = {
+  id: number;
+  category: string;
+  max: number;
+}
 
 export default function Home() {
-  const fetchInfo: (string | number)[] = [
-    {id:1, category: "general", max: 6},
-    {id:2, category: "technology", max: 3 },
-    {id:3, category: "business", max: 3 },
-    {id:4, category: "science", max: 3 },
-    {id:5, category: "health", max: 3 },
+  const fetchInfo: MyPost[] = [
+    {"id": 1, "category": "general", "max": 6},
+    {"id": 2, "category": "technology", "max": 3 },
+    {"id": 3, "category": "business", "max": 3 },
+    {"id": 4, "category": "science", "max": 3 },
+    {"id": 5, "category": "health", "max": 3 },
   ] 
   const content = (
     <main className="flex min-h-screen flex-col items-center justify-between">

0c00d5835f3fe23d343494cbcf16dd7b57daadc7
Pa3ick <66882379+pa3ick-code@users.noreply.github.com>
2024-06-05 02:59:55 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import { Suspense } from "react";
 
 export default function Home() {
   const fetchInfo = [
-    {category: "general", max: 6},
-    {category: "technology", max: 3 },
-    {category: "business", max: 3 },
-    {category: "science", max: 3 },
-    {category: "health", max: 3 },
+    {id:1, category: "general", max: 6},
+    {id:2, category: "technology", max: 3 },
+    {id:3, category: "business", max: 3 },
+    {id:4, category: "science", max: 3 },
+    {id:5, category: "health", max: 3 },
   ] 
   const content = (
     <main className="flex min-h-screen flex-col items-center justify-between">
       {
-        fetchInfo.map(({category, max}) => (
+        fetchInfo.map(({id, category, max}) => (
           <Suspence fallback={<h3>Loading...</h3>}>
-            <Posts category={category} max={max}/>
+            <Posts key={id} category={category} max={max}/>
           </Suspence>
         ))
       }
